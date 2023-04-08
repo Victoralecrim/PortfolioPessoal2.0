39557387e6d596d6ccc5d6cf1627aa8a1f635b96
Victor Alecrim <victoralecrim11@gmail.com>
2023-04-07 21:28:15 -0300

    first commit

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,16 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre Mim</h2>
+      <p>
+        Sou uma pessoa bastante dedicada e esforçada sempre buscando aperfeiçoar meus conhecimentos na area de TI e sempre estudando novas tecnologias
+      </p>
+      <br />
+      <p className="text-information-tecnology">
+        Aqui estão algumas Tecnologias em que eu estudo:
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,16 @@
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Ficou curioso ? <br />
+        Clique abaixo para ver alguns dos meus projetos
+      </p>
+      <a href="https://github.com/Victoralecrim?tab=repositories" className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
